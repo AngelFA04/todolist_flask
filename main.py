3feb1e28b49c6fa0825f32c2876a8f7e4486bfa3
Angel Flores <cyberangel1000@gmail.com>
2020-09-13 16:51:54 -0500

    Application reorganized using the module 'app'.

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import unittest
+from app import create_app
+from app.forms import LoginForm
+app = create_app()
 
-
-# A news flask app is created
-# it takes the actual file
-app = Flask(__name__)
-bootstrap =  Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'TOP_SECRET'
 
 
 todos =  ['T1', 'T2', 'T3']
 
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 @app.cli.command()
 def test():
